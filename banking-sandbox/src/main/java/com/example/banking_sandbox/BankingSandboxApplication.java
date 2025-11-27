1429b80d8843950d5b70eb72c37d19f4146f9b67
Krishna4t <krishnakrishna81086@gmail.com>
2025-11-27 14:13:57 +0530

    Intial spring boot project setup

---
+++ banking-sandbox/src/main/java/com/example/banking_sandbox/BankingSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.banking_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSandboxApplication.class, args);
+	}
+
+}
---
+++ banking-sandbox/src/test/java/com/example/banking_sandbox/BankingSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banking_sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
